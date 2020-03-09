f31e6abbf89d2a1200124a09e610d14c90e1cfb5
Diego Bezerra <diegoh@motorola.com>
2020-03-09 18:18:23 -0400

    Creating Database, Modules and Migrations
    *
    *

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # django_project
+DJANGO BLOG
 Django Project for learning purposes
+
+ ==> Following the Django tutorials (by CoreyMSchafer) on 
+https://www.youtube.com/playlist?list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p
+
+
+*****CODE SNIPTES*****
+https://github.com/CoreyMSchafer/code_snippets/tree/master/Django_Blog/snippets
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Diego Henrique',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 09, 2020'
-    },
-    {
-        'author': 'Jonh Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 10, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
